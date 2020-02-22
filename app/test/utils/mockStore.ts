4d63aa6a2b1c573a63b73269fc47b2e014f69dd9
Julio Xavier <julioxavierr@gmail.com>
2020-02-22 17:16:38 -0300

    add test helpers

---
+++ app/test/utils/mockStore.ts
@@ -0,0 +1,7 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk]; // add your middlewares like `redux-thunk`
+const mockStore = configureStore(middlewares);
+
+export default mockStore;
