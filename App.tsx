739d5f95c1c581e546bd8ee521e48268c95fb2bb
Julio Xavier <julioxavierr@gmail.com>
2020-02-22 12:39:24 -0300

    add provider on root component

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { useResources } from './app/hooks';
+import { Provider } from 'react-redux';
+import { useResources } from 'app/hooks';
+import store from 'app/store';
 import Fonts from './assets/fonts';
 
 export default function App() {
@@ -9,11 +11,13 @@ export default function App() {
   if (isLoadingResources) return null;
 
   return (
-    <View style={styles.container}>
-      <Text style={{ fontFamily: Fonts.CIRCULAR_STD_BOLD }}>
-        Open up App.tsx to start working on your app!
-      </Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text style={{ fontFamily: Fonts.CIRCULAR_STD_BOLD }}>
+          Open up App.tsx to start working on your app!
+        </Text>
+      </View>
+    </Provider>
   );
 }
 
