0fe2b411ace9db65d7dc6fd8f26cc8c775b7c6b9
Julio Xavier <julioxavierr@gmail.com>
2020-02-22 17:11:19 -0300

    move types to api service

--- app/services/api.ts
+++ app/services/api.ts
@@ -2,7 +2,21 @@ import axios from 'axios';
 
 // for now only one api is used
 const api = axios.create({
-  baseURL: 'https://opentdb.com/api.php?',
+  baseURL: 'https://opentdb.com/api.php',
 });
+
+export enum ResponseCode {
+  Success = 0,
+  NoResults = 1,
+  InvalidParameter = 2,
+  InvalidToken = 3,
+  TokenEmpty = 4,
+}
+
+export enum Difficulty {
+  Hard = 'hard',
+  Medium = 'medium',
+  Easy = 'easy',
+}
 
 export default api;
