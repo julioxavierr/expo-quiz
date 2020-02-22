bf5c1462e2dd3f625ef96c75ebc939e853c5a5e6
Julio Xavier <julioxavierr@gmail.com>
2020-02-22 11:51:05 -0300

    add app fonts

---
+++ assets/fonts/index.ts
@@ -0,0 +1,8 @@
+/* export font names just for convenience */
+
+const FONT_NAMES = {
+  CIRCULAR_STD_BOLD: 'CircularStd-Bold',
+  CIRCULAR_STD_MEDIUM: 'CircularStd-Medium',
+};
+
+export default FONT_NAMES;
