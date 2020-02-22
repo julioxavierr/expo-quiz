853157939d95811715a57bac40c7b987bbee427a
Julio Xavier <julioxavierr@gmail.com>
2020-02-22 10:22:15 -0300

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/babel',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+};
