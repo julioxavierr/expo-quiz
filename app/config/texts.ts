86721fa3fc1421e632cefcc8a58c5e64f5ce9667
Julio Xavier <julioxavierr@gmail.com>
2020-02-23 19:38:41 -0300

    update texts

--- app/config/texts.ts
+++ app/config/texts.ts
@@ -1,13 +1,25 @@
 const texts = {
   shared: {
     generalError: 'An error ocurred',
+    difficulty: {
+      hard: 'Hard',
+      medium: 'Medium',
+      easy: 'Easy',
+    },
+    options: {
+      true: 'True',
+      false: 'False',
+    },
   },
   home: {
     welcome: 'Welcome,',
     startQuestion: 'Ready for a quiz? Press start\n and be challenged!',
+    start: 'Start',
+    scored: 'You scored',
   },
-  quiz: {},
-  result: {},
+  result: {
+    playAgain: 'Play Again?',
+  },
 };
 
 export default texts;
