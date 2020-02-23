6d05943a6c4ea406600edd20af8026d4351ec0da
Julio Xavier <julioxavierr@gmail.com>
2020-02-23 18:44:11 -0300

    update texts and theme

--- app/config/texts.ts
+++ app/config/texts.ts
@@ -2,7 +2,10 @@ const texts = {
   shared: {
     generalError: 'An error ocurred',
   },
-  home: {},
+  home: {
+    welcome: 'Welcome,',
+    startQuestion: 'Ready for a quiz? Press start\n and be challenged!',
+  },
   quiz: {},
   result: {},
 };
--- app/config/theme.ts
+++ app/config/theme.ts
@@ -1,6 +1,11 @@
 const colors = {
   headerContent: '#FFF',
-  backgroundColor: ['#9C5F8A', '#282078', '#3961A9'],
+  backgroundColor: ['#9C5F8A', '#282078'],
+  text: {
+    white: '#FFF',
+    blue: '#223350',
+    grayish: '#75849A',
+  },
 };
 
 export { colors };
