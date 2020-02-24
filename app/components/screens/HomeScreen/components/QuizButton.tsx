a21352ee264e3d433464d7131339087fb0935a83
Julio Xavier <julioxavierr@gmail.com>
2020-02-24 00:45:38 -0300

    add QuizButton and QuizFlatList

---
+++ app/components/screens/HomeScreen/components/QuizButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import {} from 'styled-system';
+import { colors } from 'app/config/theme';
+import { Flex, Badge, Text } from 'app/components/common';
+
+const Touchable = styled.TouchableOpacity`
+  height: 120px;
+  width: 295px;
+  border-radius: 8px;
+  background-color: ${colors.card.backgroundColor};
+`;
+
+const QuizButton = () => {
+  return (
+    <Touchable>
+      <Flex row justifyContent="space-between" p="10px">
+        <Badge value="medium" backgroundColor={colors.difficulty.medium} />
+        <Text color={colors.text.grayish}>3 days ago</Text>
+      </Flex>
+      <Flex
+        borderTopWidth="1px"
+        borderTopColor={colors.card.separator}
+        p="10px"
+        mt="40px"
+      >
+        <Text bold>3 of 4 questions</Text>
+      </Flex>
+    </Touchable>
+  );
+};
+
+export default QuizButton;
