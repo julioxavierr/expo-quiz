a48e84b529fc2868e98005a72b2d8817a5a4ce61
Julio Xavier <julioxavierr@gmail.com>
2020-02-24 00:18:25 -0300

    add ResultScreen with mocked data

--- app/components/screens/ResultScreen/ResultScreen.tsx
+++ app/components/screens/ResultScreen/ResultScreen.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Text, Button } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from 'app/config/routes';
-import { Container } from 'app/components/common';
+import { Container, Flex, Text, Button } from 'app/components/common';
+import texts from 'app/config/texts';
+import { colors } from 'app/config/theme';
+import Answer from './components/Answer';
+import { FlatList } from 'react-native-gesture-handler';
 
 type ResultScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -16,8 +19,34 @@ type Props = {
 const ResultScreen = ({ navigation }: Props) => {
   return (
     <Container>
-      <Text>Result</Text>
-      <Button title="Home" onPress={() => navigation.popToTop()} />
+      <Flex
+        flex={1}
+        backgroundColor="white"
+        borderTopRightRadius="50px"
+        p="30px"
+      >
+        <Text size="big" bold>
+          You scored 3 of 10
+        </Text>
+        <FlatList
+          data={['mocked', 'mocked']}
+          renderItem={() => (
+            <Flex my="10px">
+              <Answer />
+            </Flex>
+          )}
+        />
+        <Flex mt="auto">
+          <Button
+            mt="15px"
+            backgroundColor={colors.button.blue}
+            textColor={colors.text.white}
+            onPress={() => navigation.popToTop()}
+          >
+            {texts.result.playAgain}
+          </Button>
+        </Flex>
+      </Flex>
     </Container>
   );
 };
