56a85e730b84147f8060969e05bc7738aca5f583
Julio Xavier <julioxavierr@gmail.com>
2020-02-24 00:15:46 -0300

    add QuizScreen with mocked data

--- app/components/screens/QuizScreen/QuizScreen.tsx
+++ app/components/screens/QuizScreen/QuizScreen.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Text, Button } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import ROUTES, { RootStackParamList } from 'app/config/routes';
-import { Container } from 'app/components/common';
+import { Container, Flex, Button, Badge, Text } from 'app/components/common';
+import { colors } from 'app/config/theme';
+import texts from 'app/config/texts';
 
 type ResultScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -16,11 +17,31 @@ type Props = {
 const QuizScreen = ({ navigation }: Props) => {
   return (
     <Container>
-      <Text>Quiz</Text>
-      <Button
-        title="Result"
-        onPress={() => navigation.navigate(ROUTES.RESULT)}
-      />
+      <Flex
+        flex={1}
+        backgroundColor="white"
+        borderTopRightRadius="50px"
+        p="30px"
+      >
+        <Badge value="1" backgroundColor={colors.difficulty.hard} />
+        <Text mt="10px">What</Text>
+        <Flex mt="auto">
+          <Button
+            textColor={colors.text.white}
+            backgroundColor={colors.button.blue}
+          >
+            {texts.shared.options.true}
+          </Button>
+          <Button
+            textColor={colors.text.white}
+            backgroundColor={colors.button.red}
+            mt="15px"
+            onPress={() => navigation.navigate(ROUTES.RESULT)}
+          >
+            {texts.shared.options.false}
+          </Button>
+        </Flex>
+      </Flex>
     </Container>
   );
 };
